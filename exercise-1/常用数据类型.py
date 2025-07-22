2d292704f11e7c2e8600fb25a732fd43db3fc20a
licong <2397334312@qq.com>
2025-07-22 17:10:32 +0800

    feat：常用类型练习完善

--- exercise-1/常用数据类型.py
+++ exercise-1/常用数据类型.py
@@ -9,13 +9,14 @@ python中常用的数据据类型
 字典-DictionAry
 
 """
+
 """变量定义 变量名=变量值"""
-a = 123
-print('类型',type(a),a)
-
-a = a - 10
-
-print(a)
+# a = 123
+# print('类型',type(a),a)
+#
+# a = a - 10
+#
+# print(a)
 
 
 """运算符"""
@@ -56,8 +57,48 @@ print(a)
 # c = a // b
 # print("7 - c 的值为：", c)
 
+
 """类型转换"""
-testChangeType = "123"
-print(type(int(testChangeType)),testChangeType)
-print(type(float(testChangeType)),testChangeType)
-print(type(str(123)))
+# testChangeType = "123"
+# print(type(int(testChangeType)),testChangeType)
+# print(type(float(testChangeType)),testChangeType)
+# print(type(str(123)))
+
+
+"""数组 List"""
+# testList = [1,2,3]
+# print(testList)
+
+
+"""元组 Tuple"""
+# # 定义之后不支持改变，使用索引获取对应项的值，
+# testTuple = (1, 2, 3)
+# print(testTuple[0])
+# # 如果元组只有一项，用逗号来消除数学公式中的歧义
+# testTuple2 = (1,)
+# print(testTuple2[0])
+
+
+"""字典DictionAry，其实就是对象"""
+# testDict = {
+#     "userName":"张三",
+#     "age":22,
+# }
+# # 通过key获取，如果没有会报错
+# print(testDict["userName"])
+# # 通过内置方法获取，如果不存在，返回None或者指定值
+# print(testDict.get("userName"))
+# print(testDict.get("userName2"))
+# print(testDict.get("userName2","指定值"))
+
+
+"""集合 Set，存储一组不重复的key的集合，无序"""
+# 自身会有一些方法
+testSet = {"a", "b", 2}
+print(testSet)
+testSet2 = {"a", "b", 2, "c", 2}
+print(testSet2)
+# 两个set可以做数学意义上的交集、并集等操作
+print(testSet & testSet2)
+print(testSet | testSet2)
+print(testSet ^ testSet2)
