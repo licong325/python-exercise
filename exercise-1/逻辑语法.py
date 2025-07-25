ca14f088330a6e1b14a506f7298c8c304a4fff43
licong <2397334312@qq.com>
2025-07-25 15:57:58 +0800

    feat：逻辑语法

---
+++ exercise-1/逻辑语法.py
@@ -0,0 +1,32 @@
+"""if else"""
+from re import match
+
+# testIf = 99
+# value = input('value:')
+# if int(value) > testIf:
+#     print('当前大于%s' % testIf)
+# else:
+#     print('当前小于%s' % testIf)
+
+
+"""match 模式匹配，类似语switch case"""
+# match语法是3.10之后的新特性
+# testMatch = '13'
+# match testMatch:
+#     case '20':
+#         print('当前年龄%s,已成年' % testMatch)
+#     case _:
+#         print('没有符合项')
+
+
+"""循环 for in """
+# testForIn = range(100) # 生成1-99的整数
+# for i in testForIn:
+#     print(i)
+
+
+"""循环 while"""
+# n = 0
+# while n < 10:
+#     n = n + 1
+#     print(n)
